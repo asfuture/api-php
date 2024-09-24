22fbeab721a0d330a69457eb6b3a78a6ace18aed
Alex Ferreira <asfuture2019@gmail.com>
2024-09-24 12:24:02 -0300

    atualizando  tabela users

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 24-Set-2024 às 15:08
+-- Tempo de geração: 24-Set-2024 às 15:23
 -- Versão do servidor: 5.7.36
 -- versão do PHP: 7.4.26
 
@@ -29,14 +29,22 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `endereco` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `nome`, `email`, `telefone`, `endereco`) VALUES
+(1, 'JoÃ£o Silva', 'joao@example.com', '123456789', 'Rua A, 123'),
+(2, 'Alex Ferreira', 'Alex@example.com', '123456789', 'Rua Ubaldo Pinto');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
