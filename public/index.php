dbc69aad1ba1056ea7cf1a4f9fccb5113bf46dd0
Alex Ferreira <asfuture2019@gmail.com>
2024-09-29 12:12:10 -0300

    Ajuste

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 // public/index.php
 
-// Permitir requisições CORS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// Permitir requisições CORS de localhost:4200
+
+// header("Access-Control-Allow-Origin: * ");
+// header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+// header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
